4496c5201b824334425c7399264b3a7e209bb3e5
gabrieltacosta <gabriel.tuka@gmail.com>
2026-04-21 20:49:01 -0300

    fix: google ads script

--- src/components/google/AdSense.tsx
+++ src/components/google/AdSense.tsx
@@ -1,16 +1,13 @@
-import Script from "next/script";
-
 type AdSenseTypes = {
   pId: string;
 };
 
 const AdSense = ({ pId }: AdSenseTypes) => {
   return (
-    <Script
+    <script
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${pId}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
     />
   );
 };
