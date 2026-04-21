666f2fabc62780ed039a2254fa30641abfe959b0
Gabriel Costa <107140329+gabrieltacosta@users.noreply.github.com>
2026-04-21 15:44:24 -0300

    Update sitemap URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const siteUrl =
-    process.env.NEXT_PUBLIC_APP_URL || "https://amigosecretosimples.com";
+    process.env.NEXT_PUBLIC_APP_URL || "https://amigosecretosimples.com.br";
 
   return {
     rules: {
@@ -10,12 +10,12 @@ export default function robots(): MetadataRoute.Robots {
       // Permite a raiz e a criação de grupos em todos os idiomas
       allow: [
         "/",
-        "/groups/new",
-        "/en/groups/new",
-        "/es/groups/new",
-        "/blog",
-        "/en/blog",
-        "/es/blog",
+        "/groups/new/",
+        "/en/groups/new/",
+        "/es/groups/new/",
+        "/blog/",
+        "/en/blog/",
+        "/es/blog/",
       ],
       // Bloqueia a indexação de grupos criados por usuários em qualquer idioma
       disallow: [
